b468d96f6c1e8d37e68376f377b4af17f28ea8cf
Aga Skorska <skorskaga@gmail.com>
2021-07-25 18:17:37 +0100

    Right slide done

--- src/components/common/Promoted/Promoted.js
+++ src/components/common/Promoted/Promoted.js
@@ -3,17 +3,36 @@ import PropTypes from 'prop-types';
 import styles from './Promoted.module.scss';
 import PromotedBox from '../../common/Promoted/PromotedBox';
 import PromotedBox2 from '../../common/Promoted/PromotedBox2';
+import Button from '../Button/Button';
 
 class Promoted extends React.Component {
   state = {
-    productHotDealsLeft: 3,
-    productHotDealsRight: 16,
+    productHotDealsLeft: 7,
+    productHotDealsRight: 6,
+    fade: 'fadein',
   };
+
+  // componentDidMount(){
+  //   setTimeout(() => {
+  //     this.setState({productHotDealsLeft: })
+  //   },3000);
+  // }
+
+  changePic(e, value, direction) {
+    e.preventDefault();
+    this.setState({ fade: 'fadeout' });
+    let number = parseInt(value);
+    setTimeout(() => {
+      this.setState({ productHotDealsRight: number + direction });
+      this.setState({ fade: 'fadein' });
+    }, 500);
+  }
 
   render() {
     const { products } = this.props;
     const { productHotDealsLeft } = this.state;
     const { productHotDealsRight } = this.state;
+    const { fade } = this.state;
 
     return (
       <div className={styles.root2}>
@@ -28,7 +47,27 @@ class Promoted extends React.Component {
               .slice(productHotDealsRight, productHotDealsRight + 1)
               .map(item => (
                 <div key={item.id} className='col-8'>
-                  <PromotedBox2 {...item} />
+                  <PromotedBox2 fade={fade} {...item} />
+                  <div className={styles.actions}>
+                    <Button
+                      className={styles.buttonSliderLeft}
+                      variant='buttonLeft'
+                      onClick={e =>
+                        this.changePic(e, this.state.productHotDealsRight, -1)
+                      }
+                    >
+                      {'<'}
+                    </Button>
+                    <Button
+                      className={styles.buttonSliderRight}
+                      variant='buttonRight'
+                      onClick={e =>
+                        this.changePic(e, this.state.productHotDealsRight, +1)
+                      }
+                    >
+                      {'>'}
+                    </Button>
+                  </div>
                 </div>
               ))}
           </div>
